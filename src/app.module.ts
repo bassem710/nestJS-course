ccc7e74d39f1fa5380998236e004c7df942f7ee2
Bassem S.Mosaad <bassemsaeed710@gmail.com>
2025-07-09 11:24:45 +0300

    Add @nestjs/config dependency and configure TypeORM in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,35 @@
-﻿import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+﻿import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 
+// Importing Entities
+import { User } from './users/user.entity';
+
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    UsersModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        entities: [User],
+        synchronize: true,
+        port: configService.get<number>('DB_PORT', 5432),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        host: configService.get<string>('DB_HOST'),
+        database: configService.get<string>('DB_NAME'),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
